b58a766347e99516db6bf5b5862ad0a84ef7bd57
AG-98 <gjergjiandrea7gmail.com>
2022-11-12 17:54:25 +0100

    update chart 2 assignment 1

--- js/assignment_1/chart2.js
+++ js/assignment_1/chart2.js
@@ -12,8 +12,9 @@ var data_scale = 8;
 // append the svg object to the body of the page
 var svg = d3.select("#multipleStackedChart")
     .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+    .attr('width', '100%')
+    .attr('viewBox', '0 0 ' + (width + margin.left + margin.right) + ' ' + (height + margin.top + margin.bottom))
+    .attr("preserveAspectRatio", "xMinYMin meet")
     .append("g")
     .attr("transform",
         "translate(" + margin.left + "," + margin.top + ")");
