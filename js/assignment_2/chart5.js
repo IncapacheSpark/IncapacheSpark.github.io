13c8dda4972573df5faa8b6037806277ca81751e
AG-98 <gjergjiandrea7gmail.com>
2022-11-12 17:52:33 +0100

    update chart 5 assignment 2

--- js/assignment_2/chart5.js
+++ js/assignment_2/chart5.js
@@ -1,68 +1,100 @@
 // set the dimensions and margins of the graph
-const margin = {top: 40, right: 150, bottom: 60, left: 30},
+const margin = {top: 40, right: 100, bottom: 60, left: 20},
     width = 700 - margin.left - margin.right,
     height = 620 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 const svg = d3.select("#bubblePlot")
     .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+    .attr('width', '100%')
+    .attr('viewBox', '0 0 ' + (width + margin.left + margin.right) + ' ' + (height + margin.top + margin.bottom))
+    .attr("preserveAspectRatio", "xMinYMin meet")
     .append("g")
     .attr("transform", `translate(${margin.left},${margin.top})`);
 
 //Read the data
 d3.csv("https://raw.githubusercontent.com/IncapacheSpark/IncapacheSpark.github.io/main/python/data/ass2.csv").then(function (data) {
 
+    // List of groups (here I have one group per column)
+    const xItem = ["Height", "Crown_height", "Crown_width", "Canopy", "Leaf_area", "Leaf_biomass"]
+
+    // List of groups (here I have one group per column)
+    const yItem = ["CO2", "Pollution"]
+
+    const zItem = Array.from(xItem)
+
+    // add the options to the button
+    d3.select("#selection-x")
+        .selectAll('myOptions')
+        .data(xItem)
+        .enter()
+        .append('option')
+        .text(function (d) {
+            return d;
+        }) // text showed in the menu
+        .attr("value", function (d) {
+            return d;
+        }) // corresponding value returned by the button
+
+    // add the options to the button
+    d3.select("#selection-y")
+        .selectAll('myOptions')
+        .data(yItem)
+        .enter()
+        .append('option')
+        .text(function (d) {
+            return d;
+        }) // text showed in the menu
+        .attr("value", function (d) {
+            return d;
+        }) // corresponding value returned by the button
+
+    // add the options to the button
+    d3.select("#selection-z")
+        .selectAll('myOptions')
+        .data(zItem)
+        .enter()
+        .append('option')
+        .text(function (d) {
+            return d;
+        }) // text showed in the menu
+        .attr("value", function (d) {
+            return d;
+        }) // corresponding value returned by the button
+
+    // When the button is changed, run the updateChart function
+    d3.select("#selection-x").on("change", function(event,d) {
+        // recover the option that has been chosen
+        const selectedOptionX = d3.select(this).property("value")
+        const selectedOptionY = d3.select("#selection-y").property("value")
+        const selectedOptionZ = d3.select("#selection-z").property("value")
+        // run the updateChart function with this selected option
+        display(selectedOptionX, selectedOptionY, selectedOptionZ)
+    })
+
+    // When the button is changed, run the updateChart function
+    d3.select("#selection-y").on("change", function(event,d) {
+        // recover the option that has been chosen
+        const selectedOptionY = d3.select(this).property("value")
+        const selectedOptionX = d3.select("#selection-x").property("value")
+        const selectedOptionZ = d3.select("#selection-z").property("value")
+        // run the updateChart function with this selected option
+        display(selectedOptionX, selectedOptionY, selectedOptionZ)
+    })
+
+    // When the button is changed, run the updateChart function
+    d3.select("#selection-z").on("change", function(event,d) {
+        // recover the option that has been chosen
+        const selectedOptionZ = d3.select(this).property("value")
+        const selectedOptionX = d3.select("#selection-x").property("value")
+        const selectedOptionY = d3.select("#selection-y").property("value")
+        // run the updateChart function with this selected option
+        display(selectedOptionX, selectedOptionY, selectedOptionZ)
+    })
+
     // ---------------------------//
-    //       AXIS  AND SCALE      //
+    //           SCALE            //
     // ---------------------------//
-
-    // find the max value for X axis viz
-    const maxAxisX = Math.max.apply(Math, data.map(function (value) {
-        return value.Height;
-    }));
-
-    // Add X axis
-    const x = d3.scaleLinear()
-        .domain([0, maxAxisX])
-        .range([0, width]).nice();
-    svg.append("g")
-        .attr("transform", `translate(0, ${height})`)
-        .call(d3.axisBottom(x).ticks(3));
-
-    // Add X axis label:
-    svg.append("text")
-        .attr("text-anchor", "end")
-        .attr("x", width)
-        .attr("y", height + 50)
-        .text("Height (m)");
-
-    // find the max value for X axis viz
-    const maxAxisY = Math.max.apply(Math, data.map(function (value) {
-        return value.CO2;
-    }));
-
-    // Add Y axis
-    var y = d3.scaleLinear()
-        .domain([0, maxAxisY])
-        .range([height, 0]).nice();
-
-    svg.append("g")
-        .call(d3.axisLeft(y));
-
-    // Add Y axis label:
-    svg.append("text")
-        .attr("text-anchor", "end")
-        .attr("x", 0)
-        .attr("y", -20)
-        .text("CO2 absorption")
-        .attr("text-anchor", "start")
-
-    // Add a scale for bubble size
-    const z = d3.scaleSqrt()
-        .domain([0, 1000])
-        .range([2, 30]);
 
     // Add a scale for bubble color
     // Color scale: give me a specie name, I return a color
@@ -82,12 +114,13 @@ d3.csv("https://raw.githubusercontent.com/IncapacheSpark/IncapacheSpark.github.i
 
     // -2- Create 3 functions to show / update (when mouse move but stay on same circle) / hide the tooltip
     const showTooltip = function (event, d) {
+        const selectedOptionX = d3.select("#selection-x").property("value")
         tooltip
             .transition()
             .duration(200)
         tooltip
             .style("opacity", 1)
-            .html("Canopy Cover (m2): " + d.Canopy)
+            .html("Canopy Cover (m2): " + d[selectedOptionX])
             .style("left", (event.pageX) + "px")
             .style("top", (event.pageY - 28) + "px")
     }
@@ -124,52 +157,107 @@ d3.csv("https://raw.githubusercontent.com/IncapacheSpark/IncapacheSpark.github.i
     //       CIRCLES              //
     // ---------------------------//
 
-    // Add dots
-    svg.append('g')
-        .selectAll("dot")
-        .data(data)
-        .join("circle")
-        .attr("class", function (d) {
-            return "bubbles " + d.Name.replaceAll(' ', '_')
-        })
-        .attr("cx", d => x(d.Height))
-        .attr("cy", d => y(d.CO2))
-        .attr("r", d => z(parseFloat(d.Canopy)))
-        .style("fill", d => myColor(d.Name))
-        // -3- Trigger the functions for hover
-        .on("mouseover", showTooltip)
-        .on("mousemove", moveTooltip)
-        .on("mouseleave", hideTooltip)
+    function display(xVal, yVal, zVal) {
 
+        d3.selectAll("g > *").remove();
+
+        // find the max value for X axis viz
+        const maxAxisX = Math.max.apply(Math, data.map(function (value) {
+            return value[xVal];
+        }));
+
+        // Add X axis
+        const x = d3.scaleLinear()
+            .domain([0, maxAxisX])
+            .range([0, width]).nice();
+        svg.append("g")
+            .attr("transform", `translate(0, ${height})`)
+            .call(d3.axisBottom(x).ticks(3));
+
+        // Add X axis label:
+        svg.append("text")
+            .attr("text-anchor", "end")
+            .attr("x", width)
+            .attr("y", height + 50)
+            .text(xVal);
+
+        // find the max value for X axis viz
+        const maxAxisY = Math.max.apply(Math, data.map(function (value) {
+            return value[yVal];
+        }));
+
+        // Add Y axis
+        const y = d3.scaleLinear()
+            .domain([0, maxAxisY])
+            .range([height, 0]).nice();
+
+        svg.append("g")
+            .call(d3.axisLeft(y));
+
+        // Add Y axis label:
+        svg.append("text")
+            .attr("text-anchor", "start")
+            .attr("x", 0)
+            .attr("y", -20)
+            .text(yVal)
+
+        // Add a scale for bubble size
+        const z = d3.scaleSqrt()
+            .domain([0, 1000])
+            .range([2, 30]);
+
+        // Add dots
+        svg.append('g')
+            .selectAll("dot")
+            .data(data)
+            .join("circle")
+            .attr("class", function (d) {
+                return "bubbles " + d.Name.replaceAll(' ', '_')
+            })
+            .attr("cx", d => x(d[xVal]))
+            .attr("cy", d => y(d[yVal]))
+            .attr("r", d => z(parseFloat(d[zVal])))
+            .style("fill", d => myColor(d.Name))
+            // -3- Trigger the functions for hover
+            .on("mouseover", showTooltip)
+            .on("mousemove", moveTooltip)
+            .on("mouseleave", hideTooltip)
+
+        addLegend()
+    }
 
     // ---------------------------//
     //       LEGEND              //
     // ---------------------------//
 
-    // Add one dot in the legend for each name.
-    const size = 20
-    const species = ["Celtis australis", "Aesculus hippocastanum", "Carpinus betulus", "Tilia cordata", "Platanus x hispanica", "Tilia x europaea"]
-    svg.selectAll("myrect")
-        .data(species)
-        .join("circle")
-        .attr("cx", 390)
-        .attr("cy", (d, i) => 10 + i * (size + 5)) // 100 is where the first dot appears. 25 is the distance between dots
-        .attr("r", 7)
-        .style("fill", d => myColor(d))
-        .on("mouseover", highlight)
-        .on("mouseleave", noHighlight)
+    function addLegend() {// Add one dot in the legend for each name.
 
-    // Add labels beside legend dots
-    svg.selectAll("mylabels")
-        .data(species)
-        .enter()
-        .append("text")
-        .attr("x", 390 + size * .8)
-        .attr("y", (d, i) => i * (size + 5) + (size / 2)) // 100 is where the first dot appears. 25 is the distance between dots
-        .style("fill", d => myColor(d))
-        .text(d => d)
-        .attr("text-anchor", "left")
-        .style("alignment-baseline", "middle")
-        .on("mouseover", highlight)
-        .on("mouseleave", noHighlight)
+        const size = 20
+        const species = ["Celtis australis", "Aesculus hippocastanum", "Carpinus betulus", "Tilia cordata", "Platanus x hispanica", "Tilia x europaea"]
+        svg.selectAll("myrect")
+            .data(species)
+            .join("circle")
+            .attr("cx", 490)
+            .attr("cy", (d, i) => 10 + i * (size + 5)) // 100 is where the first dot appears. 25 is the distance between dots
+            .attr("r", 7)
+            .style("fill", d => myColor(d))
+            .on("mouseover", highlight)
+            .on("mouseleave", noHighlight)
+
+        // Add labels beside legend dots
+        svg.selectAll("mylabels")
+            .data(species)
+            .enter()
+            .append("text")
+            .attr("x", 490 + size * .8)
+            .attr("y", (d, i) => i * (size + 5) + (size / 2)) // 100 is where the first dot appears. 25 is the distance between dots
+            .style("fill", d => myColor(d))
+            .text(d => d)
+            .attr("text-anchor", "left")
+            .style("alignment-baseline", "middle")
+            .on("mouseover", highlight)
+            .on("mouseleave", noHighlight)
+    }
+
+    display("Height", "CO2", "Canopy")
 })
