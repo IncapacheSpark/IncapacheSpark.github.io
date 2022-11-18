69a9507c92bf4cfe16b86e8703f0df789c59d4e7
Brick <bricola.andrea@gmail.com>
2022-11-18 18:22:11 +0100

    testaggio prima mappa proiezione winkel

---
+++ js/assignment_3/chart1.js
@@ -0,0 +1,46 @@
+var width = 1000;
+var height = 1100;
+
+const svg = d3.select("svg");
+const width = +svg.attr("width");
+const height = +svg.attr("height");
+
+
+const path = d3.geoPath();
+projection = d3.geoWinkel3()
+  .scale(70)
+  .center([11.1, 46.08])
+  .translate([width / 2, height / 2]);
+
+// Data and color scale
+let data = new Map()
+const colorScale = d3.scaleThreshold()
+  .domain([100000, 1000000, 10000000, 30000000, 100000000, 500000000])
+  .range(d3.schemeBlues[7]);
+
+// Load external data and boot
+Promise.all([
+d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson"),
+d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world_population.csv", function(d) {
+    data.set(d.code, +d.pop)
+})
+]).then(function(loadData){
+    let topo = loadData[0]
+
+    // Draw the map
+  svg.append("g")
+    .selectAll("path")
+    .data(topo.features)
+    .join("path")
+      // draw each country
+      .attr("d", d3.geoPath()
+        .projection(projection)
+      )
+      // set the color of each country
+      .attr("fill", function (d) {
+        d.total = data.get(d.id) || 0;
+        return colorScale(d.total);
+      })
+})
+
+
