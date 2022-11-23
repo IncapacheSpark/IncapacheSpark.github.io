e22ee7fb130da42b232719249c80bdf2d1151b19
AG-98 <gjergjiandrea7gmail.com>
2022-11-23 11:48:14 +0100

    change colors chart 2 assignment 2

--- js/assignment_2/chart2.js
+++ js/assignment_2/chart2.js
@@ -54,7 +54,7 @@ d3.csv("https://raw.githubusercontent.com/IncapacheSpark/IncapacheSpark.github.i
     // Color scale: give me a specie name, I return a color
     const color = d3.scaleOrdinal()
         .domain(["Celtis_australis", "Aesculus_hippocastanum", "Carpinus_betulus", "Tilia_cordata", "Platanus_x_hispanica", "Tilia_x_europaea"])
-        .range(["#2E86AB", "#A23B72", "#F18F01", "#C73E1D", "#3B1F2B", "#2EDA12FF"])
+        .range(['#ff595e', '#ffca3a', '#8ac926', '#1982c4', '#6a4c93', '#023047'])
 
     // Show the Y scale
     const y = d3.scaleBand()
