1815d316c6d7559178e6c0c070c01fade977bc44
AG-98 <gjergjiandrea7gmail.com>
2022-11-23 19:34:09 +0100

    chart 1 assignment 3 almost done

--- js/assignment_3/chart1.js
+++ js/assignment_3/chart1.js
@@ -1,29 +1,86 @@
-var width = 800;
-var height = 400;
-trento_center = [11.1207,46.0664]
+// set the dimensions and margins of the graph
+const margin = {top: 60, right: 30, bottom: 20, left: 0},
+    width = 600 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
-var canvas = d3.select("#choroplet_map").append("svg")
-    .attr("width", width)
-    .attr("height", height)
+trento_center = [11.1207, 46.0664]
+
+const svg = d3.select("svg");
+
+const path = d3.geoPath();
+
+const projection = d3.geoIdentity().reflectY(true)
+
+const data = new Map();
+const colorScale = d3.scaleThreshold()
+    .domain([40, 300, 500, 1000, 2000, 3100])
+    .range(d3.schemeGreens[7]);
+
+// Load external data and boot
+Promise.all([
+    d3.json("https://raw.githubusercontent.com/IncapacheSpark/IncapacheSpark.github.io/main/python/data/circoscrizioni.geojson"),
+    d3.csv("https://raw.githubusercontent.com/IncapacheSpark/IncapacheSpark.github.io/main/python/data/ass3.csv", function (d) {
+        data.set(d.circoscrizione, +d.count)
+    })]).then(function (loadData) {
+    let topo = loadData[0]
+    //console.log(topo)
+
+    projection.fitSize([width, height], topo)
+
+    // create a tooltip
+    const tooltip = d3.select("#choroplet_map_1")
+        .append("div")
+        .attr("class", "tooltip")
 
 
-d3.json("/python/data/circoscrizioni.json", function(data) {
+    let mouseOver = function (event, d) {
 
-    var group = canvas.selectAll("g")
-        .data(data.features)  //array in geojson heißt "features"
-        .enter()
-        .append("g")
-    var projection = d3.geo.mercator()
-        .scale(90000)
-        .center(trento_center)  // centers map at given coordinates
-        .translate([width / 2, height / 2]); // translate map to svg
+        tooltip
+            .transition()
+            .duration(200)
+            .style("opacity", 1);
 
-    var path = d3.geo.path().projection(projection); //hand projection to projection generator --> how to translate coordinate to pixels
+        tooltip
+            .html("<span class='tooltiptext'>"+ "Neighbourhood: "+ d.properties.nome + "<br>" + "Tree aboundance: " + d.total +"</span>")
+            .style("left", (event.pageX) + "px")
+            .style("top", (event.pageY - 28) + "px");
 
+        d3.select(this)
+            .transition()
+            .duration(200)
+            .style("opacity", 1)
+            .style("stroke", "black")
+    }
 
-    var areas = group.append("path")              //append to path to each "g" element
-        .attr("d", path)                              //data comes from path generator
-        .attr("class", "area")
-        .attr("fill", "#8AC926");
+    let mouseLeave = function () {
+        d3.selectAll("path")
+            .transition()
+            .duration(200)
+            .style("opacity", .8)
+            .style("stroke", "white")
 
+        tooltip
+            .transition()
+            .duration(200)
+            .style("opacity", 0);
+    }
+
+    // Draw the map
+    svg.append("g")
+        .selectAll("path")
+        .data(topo.features)
+        .join("path")
+        // draw each circoscrizione
+        .attr("d", d3.geoPath()
+            .projection(projection)
+        )
+        // set the color of each circoscrizione
+        .attr("fill", function (d) {
+            d.total = data.get(d.properties.nome) || 0;
+            return colorScale(d.total);
+        })
+        .style("stroke", "white")
+        .style("opacity", .8)
+        .on("mouseover", mouseOver)
+        .on("mouseleave", mouseLeave)
 });
