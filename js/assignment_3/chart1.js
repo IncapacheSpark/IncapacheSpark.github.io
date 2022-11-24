d231b6c583497b0bee9f5d1027e0a7fd82bd9641
Brick <59631949+Lorsu@users.noreply.github.com>
2022-11-24 17:56:49 +0100

    versione finale di mappa1 e mappa3

--- js/assignment_3/chart1.js
+++ js/assignment_3/chart1.js
@@ -10,37 +10,35 @@ const path = d3.geoPath();
 const projection = d3.geoIdentity().reflectY(true);
 
 const data = new Map();
-
+color_arr = d3.schemeGreens[7].slice(1);
+MAX_COUNT = 3024;
 const colorScale = d3.scaleQuantize()
-    .domain([0, 3024])
-    .range(d3.schemeGreens[7]);
+    .domain([0, MAX_COUNT])
+    .range(color_arr);
 
-// Load external data and boot
 Promise.all([
     d3.json("https://raw.githubusercontent.com/IncapacheSpark/IncapacheSpark.github.io/main/python/data/circoscrizioni.geojson"),
     d3.csv("https://raw.githubusercontent.com/IncapacheSpark/IncapacheSpark.github.io/main/python/data/ass3.csv", function (d) {
         data.set(d.circoscrizione, +d.count)
     })]).then(function (loadData) {
+
         const topo = loadData[0]
-
         projection.fitSize([width, height], topo)
-
-        const tooltip = d3.select("#choroplet_map1") // create a tooltip
+        const tooltip = d3.select("#choroplet_map1")
             .append("div")
             .attr("class", "tooltip")
 
         const mouseOver = function (event, d) {
-
-            tooltip.transition()
+            tooltip
+                .transition()
                 .duration(200)
                 .style("opacity", 1);
             tooltip
                 .transition()
                 .duration(200)
                 .style("opacity", 1);
-
             tooltip
-                .html("<span class='tooltiptext'>" + d.properties.nome + "<br>" + "Tree aboundance: " + d.total + "</span>")
+                .html("<span class='tooltiptext'>" + d.properties.nome + "<br>" + "Aboundance: " + d.total + " trees</span>")
                 .style("left", (event.pageX) + "px")
                 .style("top", (event.pageY - 28) + "px");
 
@@ -64,24 +62,20 @@ Promise.all([
                 .duration(200)
                 .style("opacity", 1)
                 .style("stroke", "white")
-
             tooltip
                 .transition()
                 .duration(200)
                 .style("opacity", 0);
         }
 
-        // Draw the map
         svg.append("g")
             .selectAll("path")
             .data(topo.features)
             .join("path")
-            // draw each circoscrizione
-            .attr("d", d3.geoPath()
+            .attr("d", d3.geoPath()     // draw each circoscrizione
                 .projection(projection)
             )
-            // set the color of each circoscrizione
-            .attr("fill", function (d) {
+            .attr("fill", function (d) {               // set the color of each circoscrizione
                 d.total = data.get(d.properties.nome) || 0;
                 return colorScale(d.total);
             })
--- js/assignment_3/chart3.js
+++ js/assignment_3/chart3.js
@@ -6,52 +6,15 @@ const margin = {top: 60, right: 30, bottom: 20, left: 0},
 trento_center = [11.1207, 46.0664]
 
 const svg = d3.select("svg");
-
 const path = d3.geoPath();
-
 const projection = d3.geoIdentity().reflectY(true)
 
 const data = new Map();
-const colorScale = d3.scaleThreshold()
-    .domain([1000, 3000, 4000, 5000, 7000, 10000, 40000, 55000])
-    .range(d3.schemeBlues[9]);
+MAX_OXYGEN = 53818.4;
+const colorScale = d3.scaleQuantize()
+    .domain([0, MAX_OXYGEN])
+    .range(d3.schemeBlues[9].slice(1))
 
-// Add color legend
-shapeWidthlegend_3 = 100;
-const labels_3 = ['0', '1000', '2000', '4000', '10000', '50000',];
-const legend_3_size = shapeWidthlegend_3*labels_3.length;
-const scaleFactor_3 = 0.8;
-const legend_3 = d3.legendColor()
-    .labels(function (d) { return labels_3[d.i]; })
-    .shapePadding(0)
-    .orient("horizontal")
-    .shapeWidth(shapeWidthlegend_3)
-    .scale(colorScale)
-    .labelAlign("start") ;
-svg.append("g")
-    .attr("class", "legendThreshold")
-    .attr("font-family", "Fira Sans, sans-serif")
-    .attr("font-size", "12px")
-    .attr("transform", `translate(${(scaleFactor_3*width - legend_3_size - (margin.left - margin.right))/2},
-                                  ${height - margin.bottom/2})`);
-
-svg.select(".legendThreshold")
-    .append("text")
-        .attr("class", "caption")
-        .attr("x", legend_3_size/2)
-        .attr("y", -20)
-        .style("font-family", "Fira Sans, sans-serif")
-        .style("font-size", "14px")
-        .attr("text-anchor", "middle")
-        .text("Oxygen (kg/yr)");
-
-svg.select(".legendThreshold")
-    .call(legend_3);
-
-//var cb = d3.colorbarV(colorScale, 20, 100);
-//svg.append('g').call(cb);
-
-// Load external data and boot
 Promise.all([
     d3.json("https://raw.githubusercontent.com/IncapacheSpark/IncapacheSpark.github.io/main/python/data/circoscrizioni.geojson"),
     d3.csv("https://raw.githubusercontent.com/IncapacheSpark/IncapacheSpark.github.io/main/python/data/ass3.csv", function (d) {
@@ -61,18 +24,15 @@ Promise.all([
 
     projection.fitSize([width, height], topo)
 
-    // create a tooltip
     const tooltip = d3.select("#choroplet_map_3")
         .append("div")
         .attr("class", "tooltip")
 
     let mouseOver = function (event, d) {
-
         tooltip
             .transition()
             .duration(200)
             .style("opacity", 1);
-
         tooltip
             .html("<span class='tooltiptext'>"+ "Neighbourhood: "+ d.properties.nome
                 + "<br>" + "Oxigen production: " + d.oxigen + " (kg/yr)"
@@ -100,14 +60,12 @@ Promise.all([
             .duration(200)
             .style("opacity", 1)
             .style("stroke", "white")
-
         tooltip
             .transition()
             .duration(200)
             .style("opacity", 0);
     }
 
-    // Draw the map
     svg.append("g")
         .selectAll("path")
         .data(topo.features)
