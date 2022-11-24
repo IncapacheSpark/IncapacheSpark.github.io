2214ca6370b2a65a622d9c27cb6e71c7daa0cf6f
Brick <59631949+Lorsu@users.noreply.github.com>
2022-11-24 16:12:47 +0100

    aggiorno chart1

--- js/assignment_3/chart1.js
+++ js/assignment_3/chart1.js
@@ -1,29 +1,83 @@
-var width = 800;
-var height = 400;
+// set the dimensions and margins of the graph
+const margin = {top: 60, right: 30, bottom: 20, left: 0},
+    width = 400 - margin.left - margin.right,
+    height = 300 - margin.top - margin.bottom;
+
 trento_center = [11.1207,46.0664]
 
-var canvas = d3.select("#choroplet_map").append("svg")
-    .attr("width", width)
-    .attr("height", height)
+const svg = d3.select("svg");
+const path = d3.geoPath();
+var projection = d3.geoIdentity().reflectY(true);
+const data = new Map();
+const colorScale = d3.scaleQuantize()
+    .domain([0, 3024])
+    .range(d3.schemeGreens[7]);
 
+// Load external data and boot
+Promise.all([
+    d3.json("https://raw.githubusercontent.com/IncapacheSpark/IncapacheSpark.github.io/main/python/data/circoscrizioni.geojson"),
+    d3.csv("https://raw.githubusercontent.com/IncapacheSpark/IncapacheSpark.github.io/main/python/data/ass3.csv", function(d) {
+        data.set(d.circoscrizione, +d.count)
+    })]).then(function(loadData){
+        let topo = loadData[0]
+        console.log("stampo topo"); console.log(topo);
 
-d3.json("/python/data/circoscrizioni.json", function(data) {
+        projection.fitSize([width, height], topo)
+        
+        const tooltip = d3.select("#choroplet_map1") // create a tooltip
+            .append("div")
+            .attr("class", "tooltip")
+        
+        svg.append("g")       // Draw the map
+            .selectAll("path")
+            .data(topo.features)
+            .enter()
+            .append("path")
+            .attr("d", d3.geoPath()
+                .projection(projection)  // draw each circoscrizione
+            )
+            .attr("fill", function (d) {     // set the color of each circoscrizione
+                //console.log(d)
+                d.total = data.get(d.properties.nome) || 0;
+                //console.log(d.total)
+                return colorScale(d.total);
+            })
+            .style("stroke", "transparent")
+            .attr("class", function(d){ return "Country" } )
+            .attr("class", function(d){ return d.properties.nome } )
+            .style("opacity", .8)
+            .on("mouseover", function (event, d) {    // what subgroup are we hovering?
+                //console.log(d.properties.nome)
+            
+                
+                const subGroupName = d.properties.nome
+                tooltip.transition()
+                    .duration(200)
+                    .style("opacity", 1);
 
-    var group = canvas.selectAll("g")
-        .data(data.features)  //array in geojson heißt "features"
-        .enter()
-        .append("g")
-    var projection = d3.geo.mercator()
-        .scale(90000)
-        .center(trento_center)  // centers map at given coordinates
-        .translate([width / 2, height / 2]); // translate map to svg
+                tooltip.html("<span class='tooltiptext'>" + "neighborhood: " + subGroupName + "<br>" + "Count: " + d.total + " trees</span>")
+                    .style("left", (event.pageX) + "px")
+                    .style("top", (event.pageY - 28) + "px");
 
-    var path = d3.geo.path().projection(projection); //hand projection to projection generator --> how to translate coordinate to pixels
+                // Reduce opacity of all rect to 0.2
+                d3.selectAll(".Country")
+                    .style("opacity", 0.2)
 
+                // Highlight all rects of this subgroup with opacity 1.
+                // It is possible to select them since they have a specific class = their name.
+                d3.selectAll("." + subGroupName)
+                    .style("opacity", 1)
 
-    var areas = group.append("path")              //append to path to each "g" element
-        .attr("d", path)                              //data comes from path generator
-        .attr("class", "area")
-        .attr("fill", "#8AC926");
+            })
+            .on("mouseleave", function () { // When user do not hover anymore
 
-});
+                tooltip.transition()
+                    .duration(200)
+                    .style("opacity", 0);
+
+                // Back to normal opacity: 1
+                d3.selectAll(".Country")
+                    .style("opacity", 1)
+            })
+        
+        });
