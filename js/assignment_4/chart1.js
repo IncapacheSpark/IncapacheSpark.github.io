1a7dc68056ef5a23b2fd556fe9b611d05241c7e8
Brick <bricola.andrea@gmail.com>
2022-12-04 16:14:37 +0100

    scheletro chart1

--- js/assignment_4/chart1.js
+++ js/assignment_4/chart1.js
@@ -9,10 +9,60 @@ const svg = d3.select("#chart")
     .append("g")
     .attr("transform", `translate(${margin.left},${margin.top})`);
 
+/*
+ESEMPIO ELEMENTO DI ARRAY DATA
+{
+  "day": "1993-01",
+  "min_temp": "-9.0",
+  "max_temp": "12.0",
+  "avg_temp": "0.44"
+}
+SPECIFICHE GRAFICO: creare uno scatterplot per ogni anno considerato. Nello scatterplot per ogni mese (asse x) va rappresentata la
+temperatura minima, media e massima. I punti dello scatterplot sono uniti. 
+*/
 
-month_labels = ["JAN", "FEB", "MAR", "APR", "MAR", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];    
-url = "/pythn/assignment_4_salorno/line_chart_dataset.csv";
-d3.csv(url).then(function(data){
-    console.log("stampo data"); console.log(data);
+month_labels = ["JAN", "FEB", "MAR", "APR", "MAR", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
+
+url = "/python/assignment_4_salorno/assignment4.csv";
+d3.csv(url, function(d){
+    return { date : d3.timeParse("%Y-%m")(d.day), value : d.value }
+}).then(function(data) {
+    console.log("stampo data")
+    console.log(data);
+
+    const x = d3.scaleTime()             
+        .domain(d3.extent(data, d => d.day.slice(5)))   //il dominio e' il mese
+        .range([ 0, width ]);
     
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+    const y = d3.scaleLinear()
+        .domain( [8000, 9200])
+        .range([ height, 0 ]);
+    svg.append("g")
+        .call(d3.axisLeft(y));
+    
+    svg                     // Add the line
+        .append("path")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "#69b3a2")
+        .attr("stroke-width", 1.5)
+        .attr("d", d3.line()
+            .x(d => x(d.date))
+            .y(d => y(d.value))
+        )
+    
+
+    svg            // Add the points
+        .append("g")
+        .selectAll("dot")
+        .data(data)
+        .join("circle")
+        .attr("cx", d => x(d.date))
+        .attr("cy", d => y(d.value))
+        .attr("r", 5)
+        .attr("fill", "#69b3a2")
 })
