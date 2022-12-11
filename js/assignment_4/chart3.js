7fe811f5d5ca8848fd339ccaad3454d561b9eb1d
Roberto <roby.divia@gmail.com>
2022-12-11 18:33:10 +0100

    Assignment 4 almost finished

--- js/assignment_4/chart3.js
+++ js/assignment_4/chart3.js
@@ -1,6 +1,6 @@
 //const color = ['#ff595e', '#ffca3a', '#8ac926', '#1982c4', '#6a4c93', '#ced4da']
 
-const margin = {top: 10, right: 30, bottom: 30, left: 40},
+const margin = {top: 50, right: 30, bottom: 30, left: 40},
     width = 750 - margin.left - margin.right,
     height = 650 - margin.top - margin.bottom;
 
@@ -14,23 +14,29 @@ const svg = d3.select("#ridgeline_chart")
 
 //const colors = {"#3B4CC0", "#B40426"}
 
-const url = "/python/assignment_4_salorno/assignment4.csv";
+const url_min_temps = "/python/data/ass_4_ridgeline_min.csv";
+const url_max_temps = "/python/data/ass_4_ridgeline_max.csv";
 
 //read data
-d3.csv(url).then(function(data) {
+Promise.all([
+  d3.csv(url_min_temps),
+  d3.csv(url_max_temps),
+]).then(function (initialize) {
 
-  data.forEach(function (element) {
-      element.month = element.day.slice(5);
-      element.year = element.day.slice(0, 4);
-  });
+  let min_temps = initialize[0];
+  let max_temps = initialize[1];
 
-  const items = ["1993", "1997", "2001", "2005", "2009", "2013", "2017", "2021"];
+
+//d3.csv(url).then(function(data) {
+
+  
+  const items_years = ["1993", "1997", "2001", "2005", "2009", "2013", "2017", "2021"];
   const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
   // add the options to the button
   d3.select("#selection")
       .selectAll('myOptions')
-      .data(items)
+      .data(items_years)
       .enter()
       .append('option')
       .text(function (d) {
@@ -40,58 +46,137 @@ d3.csv(url).then(function(data) {
           return d;
       }) // corresponding value returned by the button
 
-      console.log(data);
+
+  function display_year_filtered(selected_year=items_years[0]){
+    const categories = months;
+    const n = categories.length;
 
 
-// ###################  COPY PASTE ############### https://d3-graph-gallery.com/graph/ridgeline_basic.html //
-  // Get the different categories and count them
-  const categories = data.columns
-  const n = categories.length
+    var filtered_min_data = [];
+    var filtered_max_data = [];
 
-  // Add X axis
-  const x = d3.scaleLinear()
-    .domain([-10, 140])
-    .range([ 0, width ]);
-  svg.append("g")
-    .attr("transform", `translate(0, ${height})`)
-    .call(d3.axisBottom(x));
+    var has_nones_min = false;
+    var has_nones_max = false;
 
-  // Create a Y scale for densities
-  const y = d3.scaleLinear()
-    .domain([0, 0.4])
-    .range([ height, 0]);
+    // Filtering on selected year and removing none values (the latter is not working...)
+    min_temps.forEach((d) => {
+      
+      console.log(d);
 
-  // Create the Y axis for names
-  const yName = d3.scaleBand()
-    .domain(categories)
-    .range([0, height])
-    .paddingInner(1)
-  svg.append("g")
-    .call(d3.axisLeft(yName));
+      var data_properties = Object.keys(d).slice(1);
+      data_properties.forEach((p) => {
+        if (d[p] == 'None'){
+          has_nones_min = true;
+        }
+      });
+      if(!has_nones_min && d.year == selected_year){
+        filtered_min_data.push(d);
+      }
+    });
 
-  // Compute kernel density estimation for each column:
-  const kde = kernelDensityEstimator(kernelEpanechnikov(7), x.ticks(40)) // increase this 40 for more accurate density.
-  const allDensity = []
-  for (i = 0; i < n; i++) {
-      key = categories[i]
-      density = kde( data.map(function(d){  return d[key]; }) )
-      allDensity.push({key: key, density: density})
+    max_temps.forEach((d) => {
+      var data_properties = Object.keys(d).slice(1);
+      data_properties.forEach((p) => {
+        if (d[p] == 'None'){
+          has_nones_max = true;
+        }
+      });
+      if(!has_nones_max && d.year == selected_year){
+        filtered_max_data.push(d);
+      }
+    });
+
+    console.log('final len di filtered data is', filtered_min_data.length, filtered_max_data.length);
+
+    // Add X axis
+    const x = d3.scaleLinear()
+      .domain([-30, 35])
+      .range([ 0, width ]);
+    svg.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(d3.axisBottom(x));
+
+    // Create a Y scale for densities
+    const y = d3.scaleLinear()
+      .domain([0, 2.5])
+      .range([ height, 0]);
+
+    // Create the Y axis for names
+    const yName = d3.scaleBand()
+      .domain(categories)
+      .range([0, height])
+      .paddingInner(1)
+    svg.append("g")
+      .call(d3.axisLeft(yName));
+
+    // MIN TEMPS VALUE
+    
+    // Compute kernel density estimation for each column:
+    const kde = kernelDensityEstimator(kernelEpanechnikov(7), x.ticks(40)) // increase this 40 for more accurate density.
+    const allDensity = []
+    for (i = 0; i < n; i++) {
+        key = categories[i]
+        density = kde( filtered_min_data.map(function(d){  return d[key]; }) )
+        allDensity.push({key: key, density: density})
+    }
+
+    // Add areas
+    svg.selectAll("areas")
+      .data(allDensity)
+      .join("path")
+        .attr("transform", function(d){return(`translate(0, ${(yName(d.key)-height)})`)})
+        .datum(function(d){return(d.density)})
+        .attr("fill", " #0000FF")
+        .attr("opacity", 0.7)
+        .attr("stroke", "#000")
+        .attr("stroke-width", 0.1)
+        .attr("d",  d3.line()
+            .curve(d3.curveBasis)
+            .x(function(d) { return x(d[0]); })
+            .y(function(d) { return y(d[1]); })
+        )
+        
+
+    // MAX TEMPS VALUE
+    
+    // Compute kernel density estimation for each column:
+    const kde_max = kernelDensityEstimator(kernelEpanechnikov(7), x.ticks(40)) // increase this 40 for more accurate density.
+    const allDensity_max = []
+    for (i = 0; i < n; i++) {
+        key = categories[i]
+        density = kde_max( filtered_max_data.map(function(d){  return d[key]; }) )
+        allDensity_max.push({key: key, density: density})
+    }
+
+    // Add areas
+    svg.selectAll("areas")
+      .data(allDensity_max)
+      .join("path")
+        .attr("transform", function(d){return(`translate(0, ${(yName(d.key)-height)})`)})
+        .datum(function(d){return(d.density)})
+        .attr("fill", "#FF0000")
+        .attr("opacity", 0.7)
+        .attr("stroke", "#000")
+        .attr("stroke-width", 0.1)
+        .attr("d",  d3.line()
+            .curve(d3.curveBasis)
+            .x(function(d) { return x(d[0]); })
+            .y(function(d) { return y(d[1]); })
+        )
+
   }
 
-  // Add areas
-  svg.selectAll("areas")
-    .data(allDensity)
-    .join("path")
-      .attr("transform", function(d){return(`translate(0, ${(yName(d.key)-height)})`)})
-      .datum(function(d){return(d.density)})
-      .attr("fill", "#69b3a2")
-      .attr("stroke", "#000")
-      .attr("stroke-width", 1)
-      .attr("d",  d3.line()
-          .curve(d3.curveBasis)
-          .x(function(d) { return x(d[0]); })
-          .y(function(d) { return y(d[1]); })
-      )
+  display_year_filtered();
+
+  // When the button is changed, run the updateChart function
+  d3.select("#selection").on("change", function () {
+    
+    d3.selectAll("path").remove();
+    // recover the option that has been chosen
+    const selectedYearOption = d3.select(this).property("value")
+    // run the updateChart function with this selected option
+    display_year_filtered(selectedYearOption);
+})
 
 })
 
